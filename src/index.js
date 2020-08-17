2eed3bcc4948ece2f1be3e0f735ac55feb014533
PHILIPPE LANOUGADERE <56220680+philippe76@users.noreply.github.com>
2020-08-17 15:41:53 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+ReactDOM.render(<h1>Hello World !</h1>, document.getElementById("root"));
+
+// let h1 = document.createElement("h1");
+// h1.innerHTML = "Hello React world !";
+// document.getElementById("root").appendChild(h1);
