9baba4a6be833a4d2f9017ecc901f3a81f11ae81
PHILIPPE LANOUGADERE <56220680+philippe76@users.noreply.github.com>
2020-08-17 17:33:04 +0200

    change 'require' to 'import'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
+import React from "react";
+import ReactDOM from "react-dom";
 
 const first_name = "Filip";
 const last_name = "Lanoug";
