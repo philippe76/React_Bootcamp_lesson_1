2cd8214562b5ef90fff2e69b860d39fbd74ec9a1
PHILIPPE LANOUGADERE <56220680+philippe76@users.noreply.github.com>
2020-08-17 19:18:31 +0200

    add JSX Attributes & Styling React Elements

--- src/index.js
+++ src/index.js
@@ -3,17 +3,32 @@ import ReactDOM from "react-dom";
 
 const first_name = "Filip";
 const last_name = "Lanoug";
+const sushi = "https://images.sweetauthoring.com/recipe/43926_2091.jpg";
+const random_img = "https://picsum.photos/100/100";
 
 ReactDOM.render(
   <>
-    <h1>Hello World !</h1>
+    <h1 className="heading">Hello World !</h1>
     <p>
       my name is {first_name} {last_name}
     </p>
     {/* <p>my name is {first_name + ' ' +last_name}</p> */}
     {/* <p>my name is {`${first_name} ${last_name}`}</p> */}
     <p>my age is {40 + 3}</p>
-
+    <div>
+      <img
+        className="food-image"
+        alt="sushi"
+        src="https://images.japancentre.com/recipes/pics/1369/main/flower-sushi.jpg?1469573764"
+      />
+      <img className="food-image" alt="sushi" src={sushi} />
+      <img
+        className="food-image"
+        alt="sushi"
+        src="https://cdn.shopify.com/s/files/1/2028/9645/products/cours-sushi-la-guilde-culinaire_1024x1024.jpg?v=1571610718"
+      />
+      <img alt="random" src={random_img + "?grayscale"} />
+    </div>
     <p>Copyright {new Date().getFullYear()}</p>
   </>,
   document.getElementById("root")
